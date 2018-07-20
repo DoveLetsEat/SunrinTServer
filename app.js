398e0a06b30e972db9695da5612ac793cd34e290
jinhyeokfang <jinhyeokfang@gmail.com>
2018-07-20 20:47:22 +0900

    요금방식 옵션 추가

--- app.js
+++ app.js
@@ -19,34 +19,65 @@ const Park = mongoose.model('Park', { name: String, latitude: String, longtitude
 app.post('/pLocations', (req, res) => {
     let x = req.body.latitude / 0.0090138;
     let y = req.body.longtitude / 0.010954102;
+    let cost = req.body.cost;
 
-    Park.find((err, data) => {
-        if(err) {
-            res.header({'response': false});
-            res.header({'errorMessage': err.message});
-            return res.status(500).send();
-        }
-        let newData = [];
-        let count = 0;
-        data.forEach(element => {
-            count++;
-            let distanceSquare = ((element.latitude / 0.0090138 - x) * (element.latitude / 0.0090138 - x) + (element.longtitude / 0.010954102 - y) * (element.longtitude / 0.010954102 - y));
-            if (distanceSquare <= 9) {
-                newData.push({
-                    latitude: element.latitude,
-                    longtitude: element.longtitude,
-                    name: element.name,
-                    id: element._id,
-                    distance: Math.sqrt(distanceSquare) * 1000
-                });
+    if (cost == "무료") {
+        Park.find((err, data) => {
+            if(err) {
+                res.header({'response': false});
+                res.header({'errorMessage': err.message});
+                return res.status(500).send();
             }
-            if (count == data.length) {
-                res.header({'token': req.headers.token});
-                res.header({'response': true});
-                res.json(newData.sort((a, b) => a.distance - b.distance));
-            }
+            let newData = [];
+            let count = 0;
+            data.forEach(element => {
+                count++;
+                let distanceSquare = ((element.latitude / 0.0090138 - x) * (element.latitude / 0.0090138 - x) + (element.longtitude / 0.010954102 - y) * (element.longtitude / 0.010954102 - y));
+                if (distanceSquare <= 9) {
+                    newData.push({
+                        latitude: element.latitude,
+                        longtitude: element.longtitude,
+                        name: element.name,
+                        id: element._id,
+                        distance: Math.sqrt(distanceSquare) * 1000
+                    });
+                }
+                if (count == data.length) {
+                    res.header({'token': req.headers.token});
+                    res.header({'response': true});
+                    res.json(newData.sort((a, b) => a.distance - b.distance));
+                }
+            });
         });
-    });
+    } else {
+        Park.find({costInfo: "무료"}, (err, data) => {
+            if(err) {
+                res.header({'response': false});
+                res.header({'errorMessage': err.message});
+                return res.status(500).send();
+            }
+            let newData = [];
+            let count = 0;
+            data.forEach(element => {
+                count++;
+                let distanceSquare = ((element.latitude / 0.0090138 - x) * (element.latitude / 0.0090138 - x) + (element.longtitude / 0.010954102 - y) * (element.longtitude / 0.010954102 - y));
+                if (distanceSquare <= 9) {
+                    newData.push({
+                        latitude: element.latitude,
+                        longtitude: element.longtitude,
+                        name: element.name,
+                        id: element._id,
+                        distance: Math.sqrt(distanceSquare) * 1000
+                    });
+                }
+                if (count == data.length) {
+                    res.header({'token': req.headers.token});
+                    res.header({'response': true});
+                    res.json(newData.sort((a, b) => a.distance - b.distance));
+                }
+            });
+        });
+    }
 });
 
 app.post('/pMoreInfo', (req, res) => {
