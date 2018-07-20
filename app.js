28ba16071467c884b0d0eb9b993974237842913b
jinhyeokfang <jinhyeokfang@gmail.com>
2018-07-20 19:47:21 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/data');
+var path = require('path');
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var Park = mongoose.model('Park', { name: String, latitude: String, longtitude: String, weekdayStartTime: String, weekdayEndTime: String, satDayStartTime: String, satDayEndTime: String, holiDayStartTime: String, holiDayEndTime: String, costInfo: String, startCost: String, startTime: String, moreCost: String, moreTime: String, address: String });
+
+app.post('/pLocations', (req, res) => {
+    let x = req.body.latitude / 0.0090138;
+    let y = req.body.longtitude / 0.010954102;
+
+    Park.find((err, data) => {
+        if(err) {
+            res.header({'response': false});
+            res.header({'errorMessage': err.message});
+            return res.status(500).send();
+        }
+        let newData = [];
+        let count = 0;
+        data.forEach(element => {
+            count++;
+            let distanceSquare = ((element.latitude / 0.0090138 - x) * (element.latitude / 0.0090138 - x) + (element.longtitude / 0.010954102 - y) * (element.longtitude / 0.010954102 - y));
+            if (distanceSquare <= 9) {
+                newData.push({
+                    latitude: element.latitude,
+                    longtitude: element.longtitude,
+                    name: element.name,
+                    id: element._id,
+                    distance: Math.sqrt(distanceSquare) * 1000
+                });
+            }
+            if (count == data.length) {
+                res.header({'token': req.headers.token});
+                res.header({'response': true});
+                res.json(newData.sort((a, b) => a.distance - b.distance));
+            }
+        });
+    });
+});
+
+app.post('/pMoreInfo', (req, res) => {
+    Park.findOne({ _id: req.body.id }, (err, data) => {
+        if(err) {
+            res.header({'response': false});
+            res.header({'errorMessage': err.message});
+            return res.status(500).send();
+        }
+        res.header({'token': req.headers.token});
+        res.header({'response': true});
+        res.json(data);
+    });
+});
+
+module.exports = app;
